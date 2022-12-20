61962c8d020319b3f57c950042b0de333eb08493
SSabarinathan <ssabarinathan200109@gmail.com>
2022-12-20 18:40:31 +0530

    Directives,Types of Directives,SVG

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -3,19 +3,22 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
-  styleUrls: ['./search.component.css']
+  styleUrls: ['./search.component.css'],
 })
 export class SearchComponent implements OnInit {
+  public style = {
+    border: '10px solid black',
+  };
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  searchValue: string = '';
+
+  changeSearchValue(data: any) {
+    this.searchValue = data.target.value;
   }
-
-  searchValue: string= "Sabari";
-
-  changeSearchValue(eventData : Event){
-    this.searchValue= (<HTMLInputElement>eventData.target).value;
-  }
-
+  //for ngstyle
+  condition = true;
+  //for ngModel
 }
